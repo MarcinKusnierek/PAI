f46314a253956893364bb90ad1ca9eb99f995457
MarcinKuśnierek <marcin.kusnierek10@gmail.com>
2017-09-12 15:43:20 +0200

    Zajęcia 1

---
+++ js/funkcje.js
@@ -0,0 +1,43 @@
+/*var imie = "Adrian";
+
+function witaj()
+{
+    document.write("Witaj" + " " + imie);
+}
+witaj(imie);
+
+function poleProstokata(a, b){
+    var pole = a * b;
+    return pole;
+}
+
+var szerokosc = prompt("Podaj szerokość");
+var wysokosc = prompt("Podaj wysokosc");
+
+document.write(poleProstokata(szerokosc,wysokosc));
+
+function poleObjetosc(a, b, c)
+{
+    var pole = a * b;
+    var objetosc = pole * c;
+    var wynik = [pole, objetosc];
+return wynik;
+}
+
+console.log(poleObjetosc(2,3,5));
+var pole = poleObjetosc(2,3,10)[0];
+var objetosc = poleObjetosc(2,3,10)[1];
+console.log(pole);
+console.log(objetosc);
+
+var x = prompt("Podaj wartość","0 - pole, 1 - objetosc");
+var dane = poleObjetosc(2,3,10)[x];
+console.log(dane);*/
+
+//Napisać skrypt który oblicza obwód, pole, koła oraz objętość stożka, dane podaje użytkownik. Wykorzystaj obiekt Math. Użytkownik wybiera za pomocą pola radio co chce obliczyć. Dane wprowadza w formularzu. Dane wyświetl w bloku.
+
+
+
+
+
+
